f8700b25bae8a8e9ca17c7771f3cc995282fad3b
Safaa_mujahed <safamojahed123@gmail.com>
2019-09-14 22:28:43 +0300

    JSON file is done:) :):)

--- src/App/Json.java
+++ src/App/Json.java
@@ -1,8 +1,44 @@
 package App;
 
-import java.io.BufferedReader;
+import org.json.simple.parser.JSONParser;
+import org.json.*;
+
+
+
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.simple.parser.ParseException;
 
 public class Json {
+    Json json = new Json();
+    Adapter adapter = new Adapter(json);
 
+    public void fun() throws JSONException, ParseException, IOException {
+        adapter.convertXmlTOJson();
+
+        JSONParser parser = new JSONParser();
+        try {
+            Object object = parser
+                    .parse(new FileReader("data.xml"));
+            JSONObject jsonObject = (JSONObject)object;
+            //convert Object to JSONObject
+             jsonObject = (JSONObject) object;
+
+            //Reading the String
+            String name = (String) jsonObject.get("Name");
+            Long age = (Long) jsonObject.get("Age");
+
+
+            //Printing all the values
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
